fd9dc579b7d2601c8b3d754682fdbcd9dc8d0abd
Vipari <vipari.left@gmail.com>
2022-08-30 16:15:31 -0500

    Test 2

--- hello.go
+++ hello.go
@@ -7,12 +7,5 @@ import (
 )
 
 func main() {
-	u := mat.NewVecDense(3, []float64{1, 2, 3})
-	v := mat.NewVecDense(3, []float64{4, 5, 6})
-
-	fmt.Println(u.AtVec(1))
-	fmt.Println(v.AtVec(2))
-
-	u.AddVec(u, v)
-	fmt.Println(u.RawVector().Data)
+	fmt.Println(mat.NewVecDense(3, []float64{1, 2, 3}).RawVector().Data)
 }
